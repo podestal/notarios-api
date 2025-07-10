43971c279dc1ce40b659e5cb3cb8e0b385e028da
podestal <luisrodriguezpodesta@gmail.com>
2025-07-10 09:00:58 -0400

    change url pattern to fix bug

--- app/ducumentation/urls.py
+++ app/ducumentation/urls.py
@@ -11,6 +11,6 @@ It includes the URL patterns for the Notaria app's views.
 router = routers.DefaultRouter()
 router.register('documentos', views.DocumentosGeneradosViewSet)
 
-urlpatterns = router.urls + [
+urlpatterns = [
     path('docs/upload/', views.upload_document_to_r2, name='upload_document_to_r2'),
-]
+] + router.urls
