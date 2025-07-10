caf2fb119dff5180a1a90592e6397c2f80633e17
podestal <luisrodriguezpodesta@gmail.com>
2025-07-10 09:17:43 -0400

    placing api function on top

--- app/ducumentation/views.py
+++ app/ducumentation/views.py
@@ -28,6 +28,37 @@ from django.http import JsonResponse
 
 import re
 
+@csrf_exempt
+def upload_document_to_r2(request):
+    print("UPLOAD VIEW CALLED")
+    if request.method == 'POST':
+        file = request.FILES.get('file')
+        if not file:
+            return JsonResponse({'error': 'Missing file'}, status=status.HTTP_400_BAD_REQUEST)
+
+        object_key = f"rodriguez-zea/documentos/PROTOCOLARES/ACTAS DE TRANSFERENCIA DE BIENES MUEBLES REGISTRABLES/{file.name}"
+
+        s3 = boto3.client(
+            's3',
+            endpoint_url=os.environ.get('CLOUDFLARE_R2_ENDPOINT'),
+            aws_access_key_id=os.environ.get('CLOUDFLARE_R2_ACCESS_KEY'),
+            aws_secret_access_key=os.environ.get('CLOUDFLARE_R2_SECRET_KEY'),
+            config=Config(signature_version='s3v4'),
+            region_name='auto',
+        )
+
+        try:
+            s3.upload_fileobj(
+                file,
+                os.environ.get('CLOUDFLARE_R2_BUCKET'),
+                object_key
+            )
+            return JsonResponse({'status': 'success'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(f"Error uploading to R2: {e}")
+            return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return JsonResponse({'error': 'Invalid method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
 
 class VehicleTransferDocumentService:
     """
@@ -851,35 +882,3 @@ class DocumentosGeneradosViewSet(ModelViewSet):
         service = VehicleTransferDocumentService()
         return service.generate_vehicle_transfer_document(template_id, kardex, action)
 
-
-
-@csrf_exempt
-def upload_document_to_r2(request):
-    print("UPLOAD VIEW CALLED")
-    if request.method == 'POST':
-        file = request.FILES.get('file')
-        if not file:
-            return JsonResponse({'error': 'Missing file'}, status=status.HTTP_400_BAD_REQUEST)
-
-        object_key = f"rodriguez-zea/documentos/PROTOCOLARES/ACTAS DE TRANSFERENCIA DE BIENES MUEBLES REGISTRABLES/{file.name}"
-
-        s3 = boto3.client(
-            's3',
-            endpoint_url=os.environ.get('CLOUDFLARE_R2_ENDPOINT'),
-            aws_access_key_id=os.environ.get('CLOUDFLARE_R2_ACCESS_KEY'),
-            aws_secret_access_key=os.environ.get('CLOUDFLARE_R2_SECRET_KEY'),
-            config=Config(signature_version='s3v4'),
-            region_name='auto',
-        )
-
-        try:
-            s3.upload_fileobj(
-                file,
-                os.environ.get('CLOUDFLARE_R2_BUCKET'),
-                object_key
-            )
-            return JsonResponse({'status': 'success'}, status=status.HTTP_200_OK)
-        except Exception as e:
-            print(f"Error uploading to R2: {e}")
-            return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    return JsonResponse({'error': 'Invalid method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
