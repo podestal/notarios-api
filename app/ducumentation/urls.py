3e90238955eadf8b3f0758e9019aa485b10022e8
podestal <luisrodriguezpodesta@gmail.com>
2025-07-15 09:21:17 -0400

    updating the url pattern

--- app/ducumentation/urls.py
+++ app/ducumentation/urls.py
@@ -18,5 +18,5 @@ from .views import download_docx
 urlpatterns = [
     path('upload-docx/', views.upload_document_to_r2, name='upload_document_to_r2'),
     path('update-docx/', views.update_document_in_r2, name='update_document_in_r2'),
-    path('download/<str:kardex>/', download_docx, name='download_docx'),
+    path('download/<str:kardex>/<str:filename>/', download_docx, name='download_docx'),
 ] + router.urls
