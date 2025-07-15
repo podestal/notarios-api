fd84f6001f593e82958633b07b4855ec55a3b2b0
podestal <luisrodriguezpodesta@gmail.com>
2025-07-15 07:06:32 -0400

    fixing bugs

--- app/ducumentation/urls.py
+++ app/ducumentation/urls.py
@@ -13,7 +13,7 @@ It includes the URL patterns for the Notaria app's views.
 router = routers.DefaultRouter()
 router.register('documentos', views.DocumentosGeneradosViewSet)
 
-from app.ducumentation.views import download_docx
+from .views import download_docx
 
 urlpatterns = [
     path('upload-docx/', views.upload_document_to_r2, name='upload_document_to_r2'),
