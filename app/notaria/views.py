48de03cede181da689520bb8800a91cd61573a54
podestal <luisrodriguezpodesta@gmail.com>
2025-09-29 06:58:04 -0400

    removing validator to change conyuge

--- app/notaria/views.py
+++ app/notaria/views.py
@@ -2478,58 +2478,18 @@ class ClienteViewSet(ModelViewSet):
         conyuge_instance = instance.conyuge
 
         update_civil = idestcivil != instance.idestcivil or idestcivil == 2 or idestcivil == 4
-        # from_married_to_any = instance.idestcivil == 2 and idestcivil != 2
         from_any_to_married = idestcivil == 2 or idestcivil == 4
 
-        print("update_civil", update_civil)
-        # print("from_married_to_any", from_married_to_any)
-        print("from_any_to_married", from_any_to_married)
-
-        # I am single getting married or conviviente
-        # status = 2
-        # check the conyuge is married or conviviente
-        #  if married or conviviente dont do anything, return error
-        #  else change the status to 2 and update the conyuge
-        print("idestcivil from data", idestcivil)
         change_conyuge = conyuge_data != conyuge_instance
         if update_civil and change_conyuge:
             # update conyugue
             if from_any_to_married:
-                print("conyuge_data", conyuge_data)
                 if conyuge_data:
-                    # print("conyuge", conyuge)
+                    print("conyuge_data", conyuge_data)
                     conyuge_client = models.Cliente.objects.get(idcliente=conyuge_data)
-
-                    print("instance.conyuge", instance.conyuge)
-                    if conyuge_client.conyuge != "":
-                        print("conyuge_client already associated", conyuge_client.conyuge)
-                        instance.idestcivil = idestcivil
-                        instance.conyuge = ""
-                        instance.save()
-                        return Response(
-                            {
-                                "error": "No se puede cambiar el estado civil a casado si ya existe un conyugue asociado.",
-                                "error_code": "SPOUSE_ALREADY_ASSOCIATED",
-                            },
-                            status=400,
-                        )
-                    else:
-                        conyuge_client.conyuge = instance.idcliente
-                        conyuge_client.idestcivil = idestcivil
-                        conyuge_client.save()
-            # elif from_married_to_any:
-            #     conyuge = instance.conyuge
-            #     if conyuge:
-            #         conyuge_client = models.Cliente.objects.get(idcliente=conyuge)
-            #         conyuge_client.conyuge = ""
-            #         conyuge_client.idestcivil = idestcivil
-            #         conyuge_client.save()
-            #         # update instance
-            #         instance.conyuge = ""
-            #         instance.idestcivil = idestcivil
-            #         instance.save()
-            #         serializer = serializers.ClienteSerializer(instance)
-            #         return Response(serializer.data, status=status.HTTP_200_OK)
+                    conyuge_client.conyuge = instance.idcliente
+                    conyuge_client.idestcivil = idestcivil
+                    conyuge_client.save()
 
         return super().update(request, *args, **kwargs)
 
